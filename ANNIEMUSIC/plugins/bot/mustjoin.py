8818bb1eb621dcc4df2b1eb1bd285f92f0e15ac3
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-04-22 22:13:38 +0530

    Add files via upload

---
+++ ANNIEMUSIC/plugins/bot/mustjoin.py
@@ -0,0 +1,47 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+from ANNIEMUSIC import app
+
+
+MUST_JOIN = -1002336866046
+
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    
+    try:
+
+        try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+            
+            return
+        except UserNotParticipant:
+            
+            try:
+                chat_info = await app.get_chat(MUST_JOIN)
+                
+                if not chat_info.invite_link:
+                  
+                    link = await app.export_chat_invite_link(MUST_JOIN)
+                else:
+                    link = chat_info.invite_link
+                
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/51600957eaaf96ad308c7.jpg", 
+                    caption="๏ ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴏᴜʀ sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("๏Jᴏɪɴ๏", url=link),
+                            ]
+                        ]
+                    )
+                )
+                
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
