f4f0532c7f9cc5bb51d8a6dd3080ce53043224e6
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-04-22 23:37:46 +0530

    Add files via upload

---
+++ ANNIEMUSIC/plugins/tools/ping.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import *
+from ANNIEMUSIC import app
+from ANNIEMUSIC.core.call import JARVIS
+from ANNIEMUSIC.utils import bot_sys_stats
+from ANNIEMUSIC.utils.decorators.language import language
+from ANNIEMUSIC.utils.inline import supp_markup
+from config import BANNED_USERS
+
+
+@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_text("🏓 ᴘɪɴɢɪɴɢ ʙᴀʙʏ....")
+    
+    pytgping = await JARVIS.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    
+    await response.edit_text(
+        "ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ! 🖤\n"
+        "<b>ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:</b> <code>{}</code> <code>ms</code>\n"
+        "<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{}</code>".format(resp, UP),
+        reply_markup=supp_markup(_),
+    )
